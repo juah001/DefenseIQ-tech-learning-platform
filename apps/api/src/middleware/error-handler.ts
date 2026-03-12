f0f5fa8b0c75c8e9cf40b14d2891e24f626bb5df
Moses Juah <79539983+juah001@users.noreply.github.com>
2026-03-11 20:17:29 -0400

    Update error-handler.ts

--- apps/api/src/middleware/error-handler.ts
+++ apps/api/src/middleware/error-handler.ts
@@ -1,1 +1,13 @@
+import { Request, Response, NextFunction } from "express";
 
+export function errorHandler(
+  err: unknown,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  console.error(err);
+  return res.status(500).json({
+    message: "Internal server error"
+  });
+}
